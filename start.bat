324035c4c40d64672c71ebf375e65daf10435354
craig7351 <craig7351@hotmail.com>
2026-03-17 10:50:30 +0800

    remove no use code v2

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting Bandwidth Limiter...
-python main.py
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo Program crashed or exited with error.
-    pause
-)
--- stop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Stopping Bandwidth Limiter...
-taskkill /F /IM python.exe /FI "WINDOWTITLE eq Windows 11 Global Bandwidth Limiter*"
-echo.
-echo If the GUI was open, it should now be closed. You may need to verify if the python process is gone.
-pause
